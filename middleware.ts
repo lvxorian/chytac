c6676e8a82b55ca2d38f7251ce31c259ab2dfbcd
lvxorian <lvcasdope@gmail.com>
2026-06-23 19:51:31 +0200

    tmp: allow /api/debug in middleware

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,8 @@ export function middleware(request: NextRequest) {
   if (
     pathname.startsWith('/login') ||
     pathname.startsWith('/api/auth') ||
-    pathname.startsWith('/api/cron')
+    pathname.startsWith('/api/cron') ||
+    pathname.startsWith('/api/debug')
   ) {
     return NextResponse.next();
   }
